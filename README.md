803275b26a643fe87c00edcef050fef1cd23002a
stein <gideon@steinml.de>
2024-12-07 18:30:23 +0100

    default version

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ https://docs.anaconda.com/miniconda/install/#quick-command-line-install
 
 
 
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
 
 
 
--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@ conda create --name ai4luv_operational python=3.12
 git clone https://github.com/facefusion/facefusion
 cd facefusion
 python install.py --onnxruntime default
-conda install conda-forge::libstdcxx-ng
 
+sudo apt --fix-broken install
+sudo apt-get install portaudio19-dev
+conda install portaudio
+conda install conda-forge::libstdcxx-ng
+pip install -r requirements.txt
